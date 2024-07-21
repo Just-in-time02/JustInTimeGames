9cfe07a56de7dddb142d482602847f04e1ffb264
Just-in-time02 <Kalouzidisjustin@gmail.com>
2024-07-21 10:34:44 +0000

    fix error

--- js/classes/Sprite.js
+++ js/classes/Sprite.js
@@ -3,11 +3,12 @@ class Sprite{
     constructor(position, imageSrc){
           this.position = position
           this.image = new Image()
+          this.image.onload = () => {
+            this.loaded = true
+         }
           this.image.src = imageSrc
           this.loaded =  false
-          this.image.onload = () => {
-             this.loaded = true
-          }
+
     }
 
     draw(){
