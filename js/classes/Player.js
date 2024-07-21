e30b13d4ce8694c60c637559780221493f6cb384
Just-in-time02 <Kalouzidisjustin@gmail.com>
2024-07-21 13:34:41 +0200

    Update Player.js

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -35,7 +35,7 @@ class Player
         this.position.y += this.velocity.y
         this.sides.bottom = this.position.y + this.height
         //above bottom of canvas
-        if(this.sides.bottom + this.velocity.y < canvas.height == 230)
+        if(this.sides.bottom + this.velocity.y < canvas.height <= 230)
         {
         this.velocity.y += this.gravity
         }
